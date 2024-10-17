6d6b1e352472b91ea40fc043e13547db2edc1784
vesalov11 <mehmedvesalov1@gmail.vom>
2024-10-18 00:40:04 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Mehmed";
+
+        String last_name = "Vesalov";
+        int age = 20;
+
+        int student_number = 501223036;
+        char group_student = 'a';
+
+
+        System.out.println("Full student name: " + name + last_name);
+        System.out.println("age in student :" + age);
+        System.out.println("Number in class:"+ student_number);
+        System.out.println("gropu for student: " + group_student);
+    }
+}
